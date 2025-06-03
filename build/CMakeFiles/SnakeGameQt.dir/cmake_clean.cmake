60e7afd08642b82c4a8c34ccc640d6714a136ec1
TechUtopia05 <QFLsansi1@163.com>
2025-06-03 12:32:03 +0800

    增加一些指示

---
+++ build/CMakeFiles/SnakeGameQt.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SnakeGameQt.dir/Food.cpp.obj"
+  "CMakeFiles/SnakeGameQt.dir/Food.cpp.obj.d"
+  "CMakeFiles/SnakeGameQt.dir/GameRenderer.cpp.obj"
+  "CMakeFiles/SnakeGameQt.dir/GameRenderer.cpp.obj.d"
+  "CMakeFiles/SnakeGameQt.dir/Snake.cpp.obj"
+  "CMakeFiles/SnakeGameQt.dir/Snake.cpp.obj.d"
+  "CMakeFiles/SnakeGameQt.dir/SnakeGame.cpp.obj"
+  "CMakeFiles/SnakeGameQt.dir/SnakeGame.cpp.obj.d"
+  "CMakeFiles/SnakeGameQt.dir/main.cpp.obj"
+  "CMakeFiles/SnakeGameQt.dir/main.cpp.obj.d"
+  "SnakeGameQt.exe"
+  "SnakeGameQt.exe.manifest"
+  "SnakeGameQt.pdb"
+  "libSnakeGameQt.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SnakeGameQt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
