b45d510d27acab8eb41ad36d6a96acdb918c9014
xjh1145141919810 <xjh2024@sjtu.edu.cn>
2025-06-22 16:11:21 +0800

    new commit

--- GameRenderer.h
+++ GameRenderer.h
@@ -1,8 +1,10 @@
 #ifndef GAMERENDERER_H
 #define GAMERENDERER_H
-
 #include <QWidget>
 #include <QTimer>
+#include <QPainter>
+#include <QLinearGradient>
+#include <QRadialGradient>
 #include "SnakeGame.h"
 
 enum SnakeHeadShape {
@@ -15,7 +17,8 @@ enum SnakeHeadShape {
 enum SnakeBodyColor {
     GreenBody,
     BlueBody,
-    YellowBody
+    YellowBody,
+    RainbowBody  // 新增彩虹色
 };
 
 class GameRenderer : public QWidget {
@@ -32,17 +35,29 @@ private:
     void handleMenuKeyPress(int key);
     void handlePlayingKeyPress(int key);
     void handleGameOverKeyPress(int key);
-
+    
+    // 绘制蛇头
+    void drawSnakeHead(QPainter& painter, const QRect& headRect, const QPoint& direction);
+    // 绘制蛇身段
+    void drawSnakeSegment(QPainter& painter, const QRect& segmentRect, int segmentIndex, int totalSegments);
+    // 绘制食物
+    void drawFood(QPainter& painter, const QRect& foodRect);
+    // 绘制障碍物
+    void drawObstacle(QPainter& painter, const QRect& obstacleRect);
+    
     SnakeGame* game;
-    QTimer* gameTimer; // Declare gameTimer as a member variable
+    QTimer* gameTimer;
     QColor snakeHeadColor;
     QColor snakeBodyColor;
-
-    SnakeHeadShape selectedHeadShape = SquareHead; // Default head shape
-    SnakeBodyColor selectedBodyColor = GreenBody; // Default body color
-
+    SnakeHeadShape selectedHeadShape = CircleHead; // 默认改为圆形头部
+    SnakeBodyColor selectedBodyColor = RainbowBody; // 默认改为彩虹色
+    
     void stopGameTimer();
     void startGameTimer();
+    
+    // 食物动画相关
+    qreal foodScale = 1.0;
+    qreal foodRotation = 0.0;
+    QTimer* foodAnimationTimer;
 };
-
 #endif // GAMERENDERER_H
